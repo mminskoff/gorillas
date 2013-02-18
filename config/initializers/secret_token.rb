50e790e91e4f7ac38cd803c9a2b27dd75e2fd9fd
Craig Pottinger <craigpottinger@Medialetss-MacBook-Pro-4.local>
2013-02-17 20:12:35 -0500

    Installed Rails App with mysql db and haml template engine.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gorillas::Application.config.secret_token = '7af6e0c82ebbe1414d6cad545a1db123f5acdf724a386a9a9f202409fb6c3a2a3ed9dbf1a636681f1e2f0e95111e32c3ffa9750e8ed310414ced89cd39e6c65a'
